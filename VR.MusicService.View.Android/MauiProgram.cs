6340af70327f3f7958925dcc19619abbddb712aa
Vladimir <vova.rud.00@gmail.com>
2022-09-14 23:05:52 +0300

    Removed unused code.

--- VR.MusicService.View.Android/MauiProgram.cs
+++ VR.MusicService.View.Android/MauiProgram.cs
@@ -5,7 +5,7 @@ namespace VR.MusicService.View.Android;
 
 public static class MauiProgram
 {
-    private static IConfiguration Configuration { get; set; }
+    
 
     public static MauiApp CreateMauiApp()
     {
@@ -19,15 +19,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        var a = Assembly.GetExecutingAssembly();
-
-        using var stream = a.GetManifestResourceStream("VR.MusicService.View.Android.appsettings.json");
-
-        var config = new ConfigurationBuilder()
-            .AddJsonStream(stream)
-            .Build();
-
-        builder.Configuration.AddConfiguration(config);
+        
 
         return builder.Build();
     }
